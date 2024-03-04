28277d394be24813fb81cd8b50896adb9a81daa7
Adrian Laurissa <162112726+lackykenbel@users.noreply.github.com>
2024-03-04 11:45:07 +0200

    Create maincode.cs

---
+++ maincode.cs
@@ -0,0 +1,21 @@
+ if (heuristic == Heuristic.Scanline)
+        {
+            for (int i = observedSoFar; i < wave.Length; i++)
+            {
+                if (!periodic && (i % MX + N > MX || i / MX + N > MY)) continue;
+                if (sumsOfOnes[i] > 1)
+                {
+                    observedSoFar = i + 1;
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+  void Observe(int node, Random random)
+    {
+        bool[] w = wave[node];
+        for (int t = 0; t < T; t++) distribution[t] = w[t] ? weights[t] : 0.0;
+        int r = distribution.Random(random.NextDouble());
+        for (int t = 0; t < T; t++) if (w[t] != (t == r)) Ban(node, t);
+    }
