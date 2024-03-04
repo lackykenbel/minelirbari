461f730c0dcd2ac466485372989a23f575e5a9ea
Adrian Laurissa <162112726+lackykenbel@users.noreply.github.com>
2024-03-04 11:45:36 +0200

    Update maincode.cs

--- maincode.cs
+++ maincode.cs
@@ -19,3 +19,17 @@
         int r = distribution.Random(random.NextDouble());
         for (int t = 0; t < T; t++) if (w[t] != (t == r)) Ban(node, t);
     }
+
+       while (stacksize > 0)
+        {
+            (int i1, int t1) = stack[stacksize - 1];
+            stacksize--;
+
+            int x1 = i1 % MX;
+            int y1 = i1 / MX;
+
+            for (int d = 0; d < 4; d++)
+            {
+                int x2 = x1 + dx[d];
+                int y2 = y1 + dy[d];
+                if (!periodic && (x2 < 0 || y2 < 0 || x2 + N > MX || y2 + N > MY)) continue;
